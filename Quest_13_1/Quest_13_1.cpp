df4a35f2f740bb1c46b7782c6d21e82beb085419
Multik <ilyakondaurov5@gmail.com>
2025-09-16 13:12:00 +0300

    Добавил задание 13

---
+++ Quest_13_1/Quest_13_1.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Input vector size: ";
+    cin >> n;
+
+    vector<int> v(n);
+    cout << "Input numbers: ";
+    for (int i = 0; i < n; ++i) {
+        cin >> v[i];
+    }
+
+    int x;
+    cout << "Input number to delete: ";
+    cin >> x;
+
+    int write = 0;
+    for (int read = 0; read < v.size(); ++read) {
+        if (v[read] != x) {
+            v[write] = v[read];
+            write++;
+        }
+    }
+
+    // удаляем «хвост» с помощью pop_back()
+    while (v.size() > write) {
+        v.pop_back();
+    }
+
+    cout << "Result: ";
+    for (int num : v) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
---
+++ Quest_13_3/Quest_13_3.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    vector<int> db(20);
+    int count = 0;      // сколько элементов всего введено
+    int index = 0;      // текущая позиция для записи
+
+    while (true) {
+        int num;
+        cout << "input number: ";
+        cin >> num;
+
+        if (num == -1) {
+            cout << "output: ";
+            int printed = min(count, 20);
+            int start = (count >= 20) ? index : 0;
+
+            for (int i = 0; i < printed; ++i) {
+                cout << db[(start + i) % 20] << " ";
+            }
+            cout << endl;
+        }
+        else {
+            db[index] = num;
+            index = (index + 1) % 20;
+            count++;
+        }
+    }
+
+    return 0;
+}
